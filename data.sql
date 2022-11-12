b67f43a4cef9f6cd142cf2efc6cd94e8248d377b
OmarMWarraich <earthling986@gmail.com>
2022-11-12 13:49:40 +0500

    feat: Insert data

--- data.sql
+++ data.sql
@@ -1,6 +1,5 @@
 /* Populate database with sample data. */
 
-
 INSERT INTO animals (id,name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (1, 'Agumon', '2020-02-03', 0, true, 10.23);
 INSERT INTO animals (id,name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (2, 'Gabumon', '2018-11-15', 2, true, 8);
 INSERT INTO animals (id,name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (3, 'Pikachu', '2021-01-07', 1, false, 15.04);
@@ -13,4 +12,34 @@ INSERT INTO animals (id,name, date_of_birth, escape_attempts, neutered, weight_k
 INSERT INTO animals (id,name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (10, 'Blossom', '1998-10-13', 3, true, 17);
 INSERT INTO animals (id,name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (11, 'Ditto', '2022-05-14', 4, true, 22);
 
-   
+INSERT INTO owners (id,full_name, age) VALUES (1, 'Sam Smith', 34);
+INSERT INTO owners (id,full_name, age) VALUES (2, 'Jennifer Orwell', 19);
+INSERT INTO owners (id,full_name, age) VALUES (3, 'Bob', 45);
+INSERT INTO owners (id,full_name, age) VALUES (4, 'Melody Pond', 77);
+INSERT INTO owners (id,full_name, age) VALUES (5, 'Dean Winchester', 14);
+INSERT INTO owners (id,full_name, age) VALUES (6, 'Jodie Whittaker', 38);
+
+INSERT INTO species (id,name) VALUES (1, 'Pokemon');
+INSERT INTO species (id,name) VALUES (2, 'Digimon');
+
+/* Update the animals table by setting the species column to Digimon for all animals that have a name ending in mon. */
+
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+
+/* Update the animals table by setting the species column to Pokemon for all animals that don't have species already set. */
+
+UPDATE animals SET species_id = 1 WHERE species_id IS NULL;
+
+/* Modify the inserted animals to include owner information (owner_id) */
+
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name = 'Gabumon';
+UPDATE animals SET owner_id = 2 WHERE name = 'Pikachu';
+UPDATE animals SET owner_id = 3 WHERE name = 'Devimon';
+UPDATE animals SET owner_id = 3 WHERE name = 'Plantmon';
+UPDATE animals SET owner_id = 4 WHERE name = 'Charmander';
+UPDATE animals SET owner_id = 4 WHERE name = 'Squirtle';
+UPDATE animals SET owner_id = 4 WHERE name = 'Blossom';
+UPDATE animals SET owner_id = 5 WHERE name = 'Angemon';
+UPDATE animals SET owner_id = 5 WHERE name = 'Boarmon';
+
