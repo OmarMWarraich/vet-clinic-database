5f3273fb8c9f017866627e8d23268f3b0177a07a
OmarMWarraich <earthling986@gmail.com>
2022-11-12 16:54:45 +0500

    fix: Remove unnecessary comments

--- data.sql
+++ data.sql
@@ -12,13 +12,6 @@ INSERT INTO animals (id,name, date_of_birth, escape_attempts, neutered, weight_k
 INSERT INTO animals (id,name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (10, 'Blossom', '1998-10-13', 3, true, 17);
 INSERT INTO animals (id,name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (11, 'Ditto', '2022-05-14', 4, true, 22);
 
-/* INSERT INTO owners (id,full_name, age) VALUES (1, 'Sam Smith', 34);
-INSERT INTO owners (id,full_name, age) VALUES (2, 'Jennifer Orwell', 19);
-INSERT INTO owners (id,full_name, age) VALUES (3, 'Bob', 45);
-INSERT INTO owners (id,full_name, age) VALUES (4, 'Melody Pond', 77);
-INSERT INTO owners (id,full_name, age) VALUES (5, 'Dean Winchester', 14);
-INSERT INTO owners (id,full_name, age) VALUES (6, 'Jodie Whittaker', 38); */
-
 INSERT INTO owners (id, full_name, age) 
 VALUES (1, 'Sam Smith', 34), 
        (2, 'Jennifer Orwell', 19),
